69a7e511d349672d1e616ffcd7312f0a2d1b50d1
tinhninsuwai36-cell <tinhninsuwai36@gmail.com>
2026-08-30 14:14:08 +0700

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () =>{
   
     return(
         <>
-           <i> This is first GitHub changes. Run now!</i>
+           <i> This is first GitHub changes. Run now! Hello World  </i>
           <br/><br/>
        <div className="introText">
        We are giving the services to the churches and other religious community in Myanmar.
