c659414c2114dbbf954e9a235bd12883d998ecbf
tinhninsuwai36-cell <tinhninsuwai36@gmail.com>
2026-08-31 13:40:42 +0700

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import "./homeCss.css";
 import clientLists from "./clientList";
 import Application from "./Application";
+import fbicon from "../../assets/images/fbicon.png";
 const Home = () =>{
   
     return(
@@ -26,16 +27,16 @@ const Home = () =>{
 
 <Application />
 
-         <div class="dform">  
-    <div class="dtitle">Enter your Suggestion</div>
-    <form class="suggestionform" action="suggestioninsert.php" method="post">
+         <div className="dform">  
+    <div className="dtitle">Enter your Suggestion</div>
+    <form className="suggestionform" action="suggestioninsert.php" method="post">
     <div>
-        <textarea name="suggestion" rows="30em"></textarea></div>
+        <textarea name="suggestion" rows="30"></textarea></div>
     <input type="submit" value="Add Suggestion"/>
     </form>
-  <div class="dfacebookicon">  
+  <div className="dfacebookicon">  
     <a href="#" target="_blank">
-    <img src="images/fbicon.png" /></a></div>
+    <img src={fbicon} alt="photo" /></a></div>
 </div>
           
         </>
