ef3e0f0680dbd3149d3f2af08bc73115b298ee54
tinhninsuwai36-cell <tinhninsuwai36@gmail.com>
2026-09-05 11:28:45 +0700

    Update AppMain.jsx

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter , Routes , Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home/Home";
 import PrayerBooks from "./pages/Prayerbooks/PrayerBooks";
+import Application from "./pages/Applications/Application";
+import Contact from "./pages/Contact/Contact";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 
 const AppMain = () =>{
     return(
@@ -11,6 +14,9 @@ const AppMain = () =>{
                 <Route element={<MainLayout />}>
 <Route path="/home" element={<Home />} />
 <Route path="/prayerbooks" element={<PrayerBooks />} />
+<Route path="/application" element={<Application />} />
+<Route path="/contact" element={<Contact />} />
+<Route path="/privacypolicy" element={<PrivacyPolicy />} />
                 </Route>
             </Routes>
             </BrowserRouter>
